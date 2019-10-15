70c68d75e218831d1c9344fae7dee41dbe263d47
Eduard Ungureanu <u.eduard.email@gmail.com>
2019-10-15 15:52:42 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: Divi Fix Vendor Seo Plugin
+* Plugin URI: https://divi.tech/
+* Description: A quick fix for the WCMp Vendor SEO and Analytics 3rd party plugin
+* Version: 1.0
+* Author: Ungureanu Eduard
+* Author URI: https://divi.tech/
+**/
+
