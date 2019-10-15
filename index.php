4e4fdc98a187d4cf4884521ff64f155e2a8d6051
Eduard Ungureanu <u.eduard.email@gmail.com>
2019-10-15 16:17:39 +0300

    writing tests

--- index.php
+++ index.php
@@ -7,8 +7,23 @@
 * Author: Ungureanu Eduard
 * Author URI: https://divi.tech/
 **/
-global $post;
 
-if (is_woocommerce() && is_product()) {
-	var_dump('run code');
+defined( 'ABSPATH' ) or exit;
+
+add_action( 'plugins_loaded', 'plugin_prefix_woocommerce_check' );
+	function plugin_prefix_woocommerce_check() {
+	if( function_exists( 'is_woocommerce' ) ) { 
+		add_action( "wp_footer", "wpse_woocommerce_exists" );
+	}   
+	else {
+		add_action( "wp_footer", "wpse_woocommerce_doesnt_exist" );
+	}   
+}   
+
+function wpse_woocommerce_exists() {
+    echo "<h1>WooCommerce Exists!</h1>";
+}   
+
+function wpse_woocommerce_doesnt_exist() {
+    echo "<h1>WooCommerce Doesn't Exists!</h1>";
 }
